f118345718b6b8a7696f51eb27d3768e587b265d
Nitin23123 <166863746+Nitin23123@users.noreply.github.com>
2024-07-30 00:41:31 +0530

    added table.java
    prints a table for inputed numbers

---
+++ table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("input the number which is to be tabulated ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+
+}
